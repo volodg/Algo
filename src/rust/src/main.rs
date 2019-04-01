bcf0da1b5f6c4b84e14e4391fe16a2db03352194
Vladimir <gorbenko.vova@gmail.com>
2019-04-01 20:37:00 +0800

    add rust implementations

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,58 +1,39 @@
 use std::io;
 
-#[derive(PartialEq, Debug)]
-enum Either<T1: PartialEq, T2: PartialEq> {
-    Left(T1),
-    Right(T2),
-}
+fn jumping_on_clouds(input: &[u32]) -> u32 {
 
-impl <T1: Eq, T2: Eq> Either<T1, T2> {
+    if input.len() <= 1 {
+        return 0
+    }
 
-    fn is_right(&self) -> bool {
-        match self {
-            Either::Left(_) => false,
-            Either::Right(_) => true,
+    let mut result = 0;
+
+    let mut pos = 0;
+    while pos < (input.len() - 1) {
+        if pos + 2 < input.len() && input[pos + 2] == 0 {
+            pos += 2;
+            result += 1
+        } else if input[pos + 1] == 0 {
+            pos += 1;
+            result += 1
+        } else {
+            pos += 1;
         }
     }
 
-    fn right(&self) -> &T2 {
-        match self {
-            Either::Left(_) => panic!(),
-            Either::Right(val) => val,
-        }
-    }
-
-}
-
-fn minimum_bribes(input: &[u32]) -> Either<(), u32> {
-
-    let mut ans: u32 = 0;
-
-    for i in 0..(input.len() - 1) {
-        let a = input[i] as usize;
-        if a - 1 > i && a - 1 - i > 2 {
-            return Either::Left(())
-        }
-        for j in i..input.len() {
-            let b = input[j] as usize;
-            if a > b {
-                ans += 1;
-            }
-        }
-    }
-
-    Either::Right(ans)
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+
     #[test]
     fn test_minimum_bribes() {
-        assert_eq!(minimum_bribes(&mut vec![2, 1, 5, 3, 4]), Either::Right(3));
-        assert_eq!(minimum_bribes(&mut vec![2, 5, 1, 3, 4]), Either::Left(()));
-        assert_eq!(minimum_bribes(&mut vec![5, 4, 3, 2, 1]), Either::Left(()));
+        assert_eq!(jumping_on_clouds(&vec![0, 1, 0, 0, 0, 1, 0]), 3);
+        assert_eq!(jumping_on_clouds(&vec![0, 0, 1, 0, 0, 1, 0]), 4);
+        assert_eq!(jumping_on_clouds(&vec![0, 0, 0, 0, 1, 0]), 3);
     }
 }
 
@@ -76,15 +57,9 @@ fn main() {
         vec.map(|x| x.trim().parse::<T>().unwrap()).collect()
     }
 
-    let num = read_num::<u32>();
+    let _ = read_num::<u32>();
 
-    for _ in 0..num {
-        let _ = read_num::<u32>();
-        let mut nums = read_nums::<u32>();
-        let result = minimum_bribes(&mut nums);
-        match result {
-            Either::Left(_) => println!("Too chaotic"),
-            Either::Right(val) => println!("{}", val),
-        }
-    }
+    let nums = read_nums::<u32>();
+    let result = jumping_on_clouds(&nums);
+    println!("{}", result)
 }
--- src/swift/Algo/main.swift
+++ src/swift/Algo/main.swift
@@ -1,46 +1,170 @@
 import Foundation
 
-func luckBalance(k: Int, contests: [[Int]]) -> Int {
-  
-  let (zeros, nonZeros) = contests.reduce(([Int](), [Int]())) { (acc, record) in
-    if record[1] == 0 {
-      return (acc.0 + [record[0]], acc.1)
-    }
-    return (acc.0, acc.1 + [record[0]])
+func nmax(_ num: Int, _ a: Int, _ b: Int) -> Int {
+  if (a ^ num) > (b ^ num) {
+    return a
   }
-  
-  let sortedNonZeros = nonZeros.sorted { (a, b) -> Bool in
-    return a > b
-  }
-  
-  var sum = 0
-  for el in zeros {
-    sum += el
-  }
-  var index = 0
-  while index < sortedNonZeros.count && index < k {
-    sum += sortedNonZeros[index]
-    index += 1
-  }
-  
-  while index < sortedNonZeros.count {
-    sum -= sortedNonZeros[index]
-    index += 1
-  }
-  
-  return sum
+  return b
 }
 
-//test 2
+final class Trie {
+  var left: Trie? = nil
+  var right: Trie? = nil
+  var end: Bool = false
+  var _max = 0
+  
+  func append(_ val: Int) {
+    if val > _max {
+      _max = val
+    }
+    
+    if val == 0 {
+      end = true
+    } else {
+      let bit = val & 1
+      if bit == 0 {
+        let left = self.left ?? Trie()
+        self.left = left
+        left.append(val >> 1)
+      } else {
+        let right = self.right ?? Trie()
+        self.right = right
+        right.append(val >> 1)
+      }
+    }
+  }
+  
+  func maxXor(_ num: Int, myBit: Int) -> Int {
+    var subMax1: Int?
+    if end {
+      subMax1 = myBit
+    }
+    
+    if (right ?? left) == nil {
+      return myBit
+    }
+    
+    var subMax = 0
+    
+    let mmmax = _max << 1 + myBit
+    
+    let next_bit = num & 2
+    if next_bit == 0 {
+      let nmyBit = (right != nil) ? 1 : 0
+      subMax = (right ?? left)!.maxXor(num >> 1, myBit: nmyBit) << 1 + myBit
+    } else {
+      let nmyBit = (left != nil) ? 0 : 1
+      subMax = (left ?? right)!.maxXor(num >> 1, myBit: nmyBit) << 1 + myBit
+    }
+    
+    if let subMax1 = subMax1 {
+      let yy = nmax(num, subMax, subMax1)
+      return nmax(num, yy, mmmax)
+    }
+    //return subMax
+    return nmax(num, subMax, mmmax)
+  }
+  
+  func maxXor(_ num: Int) -> Int {
+    if num == 0 {
+      return _max
+    }
+    
+    var subMax1: Int?
+    if end {
+      subMax1 = 0
+    }
+    
+    var y = 0
+    
+    let bit = num & 1
+    if bit == 0 {
+      let nmyBit = (right != nil) ? 1 : 0
+      y = (right ?? left)?.maxXor(num, myBit: nmyBit) ?? 0
+    } else {
+      let nmyBit = (left != nil) ? 0 : 1
+      y = (left ?? right)?.maxXor(num, myBit: nmyBit) ?? 0
+    }
+    
+    let yy = nmax(num, y, _max)
+    if let subMax1 = subMax1 {
+      return nmax(num, yy, subMax1)
+    }
+    return yy
+  }
+}
+
+let trie = Trie()
+
+func maxXor(arr: [Int], queries: [Int]) -> [Int] {
+  
+  arr.forEach { val in
+    trie.append(val)
+  }
+  
+  //return queries.map { trie.maxXor($0) ^ $0 }
+  return queries.map { trie.maxXor($0) }
+}
+
+func maxXor_old(arr: [Int], queries: [Int]) -> [Int] {
+  return queries.map { query -> Int in
+    var max = 0
+    var num: Int?
+    for el in arr {
+      let curr = el ^ query
+      if curr > max {
+        max = curr
+        num = el
+      }
+    }
+    return num ?? arr[0]
+    //return max
+  }
+}
+
+
+//test 1
+
+//var strings = [
+//  "4",
+//  "1 3 5 7",
+//  "2",
+//  "17",
+//  "6"
+//]//[22, 7] - [7, 1]
+
+//var strings = [
+//  "5",
+//  "5 1 7 4 3",
+//  "2",
+//  "2",
+//  "0"
+//]//[7, 7] - [5, 7]
+
+//var strings = [
+//  "3",
+//  "0 1 2",
+//  "3",
+//  "3",
+//  "7",
+//  "2"
+//]//[3, 7, 3] - [0, 0, 1]
+
 var strings = [
-  "6 3",//+
-  "5 1",//+
-  "2 1",//+
-  "1 1",//-
-  "8 1",//+
-  "10 0",//+
-  "5 0"//+
-]//29
+  "1",
+  "1 2",//"8"
+  "10",//index
+//  "3",
+//  "7",
+//  "2",
+//  "1",
+//  "4",
+//  "5",
+//  "6",
+  "23423423",
+//  "9",
+//  "19",
+]//[3, 7, 3] - [0, 0, 1]
 
 var index = 0
 
@@ -53,29 +177,33 @@ func readLine2() -> String? {
   return result
 }
 
-guard let nkTemp = readLine2() else { fatalError("Bad input") }
-let nk = nkTemp.split(separator: " ").map{ String($0) }
-
-guard let n = Int(nk[0].trimmingCharacters(in: .whitespacesAndNewlines))
+guard let n = Int((readLine2()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
   else { fatalError("Bad input") }
 
-guard let k = Int(nk[1].trimmingCharacters(in: .whitespacesAndNewlines))
-  else { fatalError("Bad input") }
-
-let contests: [[Int]] = AnyIterator{ readLine2() }.prefix(n).map {
-  let contestsRow: [Int] = $0.split(separator: " ").map {
-    if let contestsItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
-      return contestsItem
-    } else { fatalError("Bad input") }
-  }
-  
-  guard contestsRow.count == 2 else { fatalError("Bad input") }
-  
-  return contestsRow
+guard let arrTemp = readLine2() else { fatalError("Bad input") }
+let arr: [Int] = arrTemp.split(separator: " ").map {
+  if let arrItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+    return arrItem
+  } else { fatalError("Bad input") }
 }
 
-guard contests.count == n else { fatalError("Bad input") }
+//guard arr.count == n else { fatalError("Bad input") }
 
-let result = luckBalance(k: k, contests: contests)
+guard let m = Int((readLine2()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+  else { fatalError("Bad input") }
 
-print(result)
+let queries: [Int] = AnyIterator{ readLine2() }.prefix(m).map {
+  if let queriesItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+    return queriesItem
+  } else { fatalError("Bad input") }
+}
+
+//guard queries.count == m else { fatalError("Bad input") }
+
+let result1 = maxXor_old(arr: arr, queries: queries)
+print(result1)
+
+let result2 = maxXor(arr: arr, queries: queries)
+print(result2)
+
+//assert(result1 == result2)
